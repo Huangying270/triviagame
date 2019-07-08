f8f8cb5076a2408c7639fb1cdf6fa0f9a76ae038
Ying Huang <huangying270@gmail.com>
2019-07-08 02:13:04 -0400

    pseudo code in js, linked css js to html

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// variables: timer, correct, incorrect, unanswered
+// array of questions and answers
+// only 1 answer can be selected
+// timer starts when start button is clicked
+// game resets when its over
