1bc017146fab62d0b2e9a7d06a4f7c6b057b1d40
Ying Huang <huangying270@gmail.com>
2019-07-08 22:54:07 -0400

    added variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,22 @@
-// variables: timer, correct, incorrect, unanswered
+$(document).ready(function(){
+
+    var time = 5;
+    var answerChosen = false;
+    var determining = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+
+
+
+
+
+
+
+})
+
+// variables: timer, correct, incorrect, unanswered, chosen answer, determining correct answer
 // array of questions and answers
 // only 1 answer can be selected
 // timer starts when start button is clicked
