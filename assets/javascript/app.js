3883879ea27a7611f29e45749232be6263fc7033
Ying Huang <huangying270@gmail.com>
2019-07-08 23:48:42 -0400

    got trivia to display on html after clicking start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
+    // question number 0 is the first question of the array as well as the answers
+    var questionNumber = 0;
 
     var question = ["1","2","3"];
     var answer = ["1","2","3"];
@@ -14,7 +16,47 @@ $(document).ready(function(){
     var cChoice = ["3","3","3"];
     var dChoice = ["4","4","4"];
 
+    function showArrays() {
+        $("#question").show();
+        $("#Choice1").show();
+        $("#Choice2").show();
+        $("#Choice3").show();
+        $("#Choice4").show();
+    }
+    
+    function hideArrays() {
+        $("#question").hide();
+        $("#Choice1").hide();
+        $("#Choice2").hide();
+        $("#Choice3").hide();
+        $("#Choice4").hide();
+    }
 
+    function hideResults() {
+        $("#totalCorrect").hide();
+        $("#totalIncorrect").hide();
+        $("#totalUnanswer").hide();
+        $("#Restart").hide();
+    }
+    
+    function displayTrivia() {
+        hideResults();
+        $("#answer").hide();
+        $("#timer").show();
+        showArrays();
+        $("#question").text(question[questionNumber]);
+        $("#Choice1").text(aChoice[questionNumber]);
+        $("#Choice2").text(bChoice[questionNumber]);
+        $("#Choice3").text(cChoice[questionNumber]);
+        $("#Choice4").text(dChoice[questionNumber]);
+    }
+    // logged onto console without questions or answers
+    console.log(displayTrivia);
+    
+    $(".start").on("click", function(){
+        displayTrivia();
+    })
+    // trivia gets displayed, need to make choices clickable and move to next question
 
 
 
